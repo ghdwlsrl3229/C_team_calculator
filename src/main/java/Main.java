e6ee34a04900a0f444212975617b01e2d72db79b
ParkJongNam <gggl.park@gmail.com>
2024-04-26 14:01:52 +0900

    sumsum and divide

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,4 +3,13 @@ public class Main {
         System.out.println("HI");
         System.out.println("jongnam");
     }
+
+    public int getSumSum(int a, int b, int c){
+        return (a + b + c);
+    }
+
+    public int getDivide(int a, int b){
+        return a / b;
+    }
+
 }
